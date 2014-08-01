0efd4596b70684830a8f730e8a69bd570057b4fc
Jeff Lee <leejefon@gmail.com>
2014-08-01 00:40:28 -0700

    Updated gulp to minify JS and CSS + Fixed minor bugs in game

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,45 @@
 var gulp = require('gulp');
+var rjs = require('gulp-requirejs');
+var uglifyJs = require('gulp-uglify');
+var minifyCSS = require('gulp-cssmin');
+var concat = require('gulp-concat');
 
 var paths = {
 	target: '.tmp/public',
-	assets: 'assets/**',
-	assetsToWatch: ['assets/**', '!assets/js/vendor/**']
+	assets: [
+		'assets/**',
+		'!assets/js/angular/**/*.js',
+		'!assets/styles/*.css'
+	],
+	assetsToWatch: [
+		'assets/**',
+		'!assets/js/vendor/**'
+	]
 };
+
+var cssFiles = [
+	'assets/js/vendor/angular-loading-bar/build/loading-bar.min.css',
+	'assets/js/vendor/toastr/toastr.min.css',
+	'assets/styles/style.css'
+];
+
+gulp.task('uglifyJs', function () {
+	rjs({
+		baseUrl: "assets/js/angular",
+		name: "TestLegends",
+		mainConfigFile: "assets/js/angular/TestLegends.js",
+		out: "testlegends.min.js"
+	})
+	.pipe(uglifyJs())
+	.pipe(gulp.dest(paths.target + '/js/angular'));
+});
+
+gulp.task('minifyCSS', function () {
+	gulp.src(cssFiles)
+		.pipe(concat('style.min.css'))
+        .pipe(minifyCSS())
+        .pipe(gulp.dest(paths.target + '/styles'));
+});
 
 gulp.task('compileAssets', function () {
 	return gulp.src(paths.assets)
@@ -12,7 +47,7 @@ gulp.task('compileAssets', function () {
 });
 
 gulp.task('watch', function () {
-	gulp.watch(paths.assetsToWatch, ['compileAssets']);
+	gulp.watch(paths.assetsToWatch, ['uglifyJs', 'minifyCSS', 'compileAssets']);
 });
 
-gulp.task('default', ['compileAssets', 'watch']);
+gulp.task('default', ['uglifyJs', 'minifyCSS', 'compileAssets', 'watch']);
